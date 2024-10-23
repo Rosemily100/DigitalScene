e6d8d33e3104f43fbe405e9f3d951e1fa58aa589
Rosemily100 <s_rosemily.baezorduno@ousd.org>
2024-10-23 10:27:25 -0700

    fix trees

--- myscene.py
+++ myscene.py
@@ -1,5 +1,5 @@
 from turtle import *
-speed(13)
+from random import randrange
 
 def sky():
     w = window_width()
@@ -41,6 +41,76 @@ def sky():
             r = 0
         if b < 0:
             b = 0
+    g=128
+    for i in range(265):
+        pencolor(0,g,0)
+        goto(x,y)
+        setheading(0)
+        fd(w)
+        bk(w)
+        y=y-1
+        r=r-1
+        b=b-1
+        if r < 0:
+            r = 0
+        if g < 0:
+            g = 0
   
-sky()
+
+
+def draw_shape(side_length,num_sides):
+    penup()
+    pendown()
+    colormode(255)
+    width(1)
+    pencolor(0,255,0)
+    angle = 360/num_sides
+    fillcolor(0,255,0)
+    begin_fill()
+    for i in range(num_sides):
+        fd(side_length)
+        left(angle)
+    end_fill()
+    penup()
+
+def draw_tree(x,y,side_length,num_tri):
+    penup()
+    for i in range(num_tri):
+        goto(x,y)
+        draw_shape(side_length,3)
+        lastX = x
+        lastY = y
+        last_side_length = side_length
+        x = x - side_length/3
+        y = y - side_length
+        side_length = side_length + side_length/1.5
+    goto(lastX + (last_side_length/5)*2,lastY)
+    fillcolor("brown")
+    begin_fill()
+    forward(last_side_length/5)
+    rt(90)
+    forward(last_side_length/3)
+    rt(90)    
+    forward(last_side_length/5)
+    rt(90)
+    forward(last_side_length/3)
+    rt(90)
+    end_fill()
+
+
+def draw_forest():
+    num_trees = 20
+    w = window_width()
+    h = window_height()
+    for i in range(num_trees):
+        x = randrange(-400,400)
+        y = randrange(-400,0)
+        l = randrange(5,15)
+        t = randrange(2,5)
+        draw_tree(x,y,l,t)
+
+speed(13)
+#sky()
+draw_forest()
 done()
+
